61e422ff4ec080cc4e4b72d0c5bade517b36cfe9
ASWELL1212 <rencru30@icloud.com>
2026-07-25 10:48:53 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,18 @@
-const CACHE = "aswell-tire-v1";
-const ASSETS = ["./","./index.html","./style.css","./app.js","./data.js","./manifest.webmanifest"];
-self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(r => r || fetch(event.request))));
+const CACHE = "asw-tire-manager-v2-1";
+const ASSETS = ["./","./index.html","./style.css","./app.js","./seed-data.js","./manifest.webmanifest"];
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", event => {
+  if (event.request.url.includes("firestore.googleapis.com") || event.request.url.includes("identitytoolkit.googleapis.com") || event.request.url.includes("gstatic.com/firebasejs")) return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(c => c.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
